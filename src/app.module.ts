2bbb89c6dc81e83035e615b39affe077cecfe8b5
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-04-14 17:36:41 -0300

    feat: using forRootAsync

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { UsersModule } from './api/users/users.module';
-import {DatabaseProvider} from './infra/db/database';
+import DatabaseProvider from './infra/db/database';
 
 //TODO:
 @Module({
   imports: [
-    TypeOrmModule.forRoot(DatabaseProvider),
+    TypeOrmModule.forRootAsync({
+      useFactory: DatabaseProvider,
+    }),
     UsersModule,
   ],
 })
--- src/infra/db/database.ts
+++ src/infra/db/database.ts
@@ -1,15 +1,20 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
 
-
-export const DatabaseProvider: TypeOrmModuleOptions = {
+export default (): TypeOrmModuleOptions => {
+  const DatabaseProvider: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: 'postgres',
+    password: '1234',
     database: 'booktop',
-    entities: [join(__dirname, '..', '..', 'api', '**', 'entities', '*.entity.{ts,js}')],
+    entities: [
+      join(__dirname, '..', '..', 'api', '**', 'entities', '*.entity.{ts,js}'),
+    ],
     migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
     synchronize: false,
   };
+
+  return DatabaseProvider;
+};
