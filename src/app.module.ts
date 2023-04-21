cc3819708f13af2c10ea387589357a90d7b2b5c3
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-04-21 12:44:56 -0300

    feat: use nest-config to get database envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './app/user/user.module';
 import DatabaseProvider from './infra/db/database';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: DatabaseProvider,
     }),
--- src/infra/db/database.ts
+++ src/infra/db/database.ts
@@ -4,11 +4,11 @@ import { join } from 'path';
 export default (): TypeOrmModuleOptions => {
   const DatabaseProvider: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'booktop',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [
       join(__dirname, '..', '..', 'api', '**', 'entities', '*.entity.{ts,js}'),
     ],
