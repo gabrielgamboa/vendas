d0ffefa64444095892a6e7418adc0d17e0473d52
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-05-03 15:35:08 -0300

    refactor: change not found error in find user

--- src/app/user/users.service.ts
+++ src/app/user/users.service.ts
@@ -63,7 +63,7 @@ export class UserService {
       where: { email },
     });
 
-    if (!user) throw new NotFoundException('user not found');
+    if (!user) throw new NotFoundException('User not found');
 
     return user;
   }
