82de7c7c37b6f84d113e3987ccdeb7763fb4b49a
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-05-17 15:24:25 -0300

    refactor: remove unused imports

--- src/app/cart/cart.controller.ts
+++ src/app/cart/cart.controller.ts
@@ -8,8 +8,6 @@ import {
   Delete,
 } from '@nestjs/common';
 import { CartService } from './cart.service';
-import { CreateCartDto } from './dtos/create-cart.dto';
-import { UpdateCartDto } from './dtos/update-cart.dto';
 
 @Controller('cart')
 export class CartController {
